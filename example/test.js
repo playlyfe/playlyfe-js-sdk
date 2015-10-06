bc7ef0cdf740b546c15f4968a35803a80e195bdd
Peter John <peter@playlyfe.com>
2015-10-06 19:19:26 +0530

    fix(examples): add infinite expires for jwt example so users can test it out

--- example/test.js
+++ example/test.js
@@ -1,4 +1,9 @@
 jwt = require('jsonwebtoken');
-token = jwt.sign({ player_id: 'student1', scopes: [] }, 'ZjIwMzI0NDEtMDlhMi00YWEwLTk1NjItNTc0MGMyOGU1MmI1OGFhM2Q2ODAtZmVlMC0xMWU0LWFlMDktYjk2NGFlZTI3MTBl', { algorithm: 'HS256', expiresInSeconds: 3600 })
-token = 'MmRkYmJkZWItY2M0My00NTNlLWFiZDEtYWZiNTIxYTgwYjIx' + ':' + token
+client_id = 'NDgyM2RkZWUtYTdiYi00Njg5LWJlZGEtODA4OWY1MTZkYWEx'
+client_secret = 'MzdlZjdmNTAtYjg5Ni00NmVhLWE5NzAtMGFkYTE0ZjRjYzY1MDZiNjQ3NjAtNmMyZi0xMWU1LTg4YzctMzlhNGYwZTRhODNh'
+token = jwt.sign({ player_id: 'student1', scopes: [] },
+  client_secret,
+  { algorithm: 'HS256', expiresIn: 3600000000 }
+)
+token = client_id + ':' + token
 console.log(token);
